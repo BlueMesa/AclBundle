b685cd6f0d70c7fb899b9e4ba1397bee7a8eccec
Radoslaw Ejsmont <radoslaw@ejsmont.net>
2015-10-27 18:59:05 +0100

    DI debugging

--- Doctrine/SecureObjectManager.php
+++ Doctrine/SecureObjectManager.php
@@ -83,7 +83,7 @@ class SecureObjectManager extends ObjectManager implements SecureObjectManagerIn
      */
     public function setUserProvider(UserProviderInterface $userProvider)
     {
-        echo "Setting User Provided!";
+        echo "Setting UserProvider to: " . get_class($userProvider) . "\n";
         $this->userProvider = $userProvider;
     }
     
@@ -96,7 +96,7 @@ class SecureObjectManager extends ObjectManager implements SecureObjectManagerIn
      */
     public function setAclProvider(MutableAclProviderInterface $aclProvider)
     {
-        echo "Setting Acl Provided!";
+        echo "Setting AclProvider to: " . get_class($aclProvider) . "\n";
         $this->aclProvider = $aclProvider;
     }
     
@@ -109,6 +109,7 @@ class SecureObjectManager extends ObjectManager implements SecureObjectManagerIn
      */
     public function setSecurityContext(SecurityContextInterface $securityContext = null)
     {
+        echo "Setting SecurityContext to: " . get_class($securityContext) . "\n";
         $this->securityContext = $securityContext;
     }
     
